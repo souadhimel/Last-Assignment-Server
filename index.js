50e5ff755b8a9690e4b37328afe7c83722a7a592
souadhimel <souad.anam@gmail.com>
2022-05-27 23:14:25 +0600

    single service got

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const cors=require('cors');
 require('dotenv').config();
 const app=express();
+const ObjectId = require("mongodb").ObjectId;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port=process.env.PORT || 5000;
 
@@ -9,7 +10,8 @@ const port=process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7bquc.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.7bquc.mongodb.net:27017,cluster0-shard-00-01.7bquc.mongodb.net:27017,cluster0-shard-00-02.7bquc.mongodb.net:27017/?ssl=true&replicaSet=atlas-uw0a6t-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
@@ -18,6 +20,8 @@ async function run(){
         await client.connect();
         const serviceCollection = client.db('furnitureMart').collection('services');
         
+
+        // getting services
         app.get('/services', async(req, res) => {
             const query={};
             const cursor=serviceCollection.find(query);
@@ -27,8 +31,13 @@ async function run(){
         });
 
 
-
-
+        // getting single service
+        app.get('/services/:id', async (req, res) => {
+            const id=req.params.id;
+            const query={_id:ObjectId(id)};
+            const service=await serviceCollection.findOne(query);
+            res.send(service);
+        });
 
 
     }
