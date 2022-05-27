8621272ee7593a6b2e513d6d0cd7793e5683a30c
souadhimel <souad.anam@gmail.com>
2022-05-27 21:10:07 +0600

    service added

--- index.js
+++ index.js
@@ -11,12 +11,34 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7bquc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+
+    try{
+        await client.connect();
+        const serviceCollection = client.db('furnitureMart').collection('services');
+        
+        app.get('/services', async(req, res) => {
+            const query={};
+            const cursor=serviceCollection.find(query);
+            const services=await cursor.toArray();
+            res.send(services);
+
+        });
+
+
+
+
+
+
+    }
+    finally{
+
+
+    }
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
    res.send('Hey, running last assignment!') 
