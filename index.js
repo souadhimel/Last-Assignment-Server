fcd8449cacc542e967a4f92e1a43a14e2b5a7336
souadhimel <souad.anam@gmail.com>
2022-05-28 10:15:23 +0600

    deleted orders

--- index.js
+++ index.js
@@ -92,7 +92,12 @@ async function run(){
       });
 
     // cancel an order
-   
+    app.delete("/myOrders/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await orderCollection.deleteOne(query);
+        res.json(result);
+      });
 
     }
     finally{
