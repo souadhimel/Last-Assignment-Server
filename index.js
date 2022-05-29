f3363eebd431226c3585a84c14bca0067ac2beb5
souadhimel <souad.anam@gmail.com>
2022-05-29 16:28:22 +0600

    updated

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.bv0uy.mongodb.net:27017,cluster0-shard-00-01.bv0uy.mongodb.net:27017,cluster0-shard-00-02.bv0uy.mongodb.net:27017/?ssl=true&replicaSet=atlas-ot6iab-shard-0&authSource=admin&retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -24,6 +23,10 @@ async function run(){
         const serviceCollection=client.db('carBiz').collection('service')
         const orderCollection=client.db('carUser').collection('order')
         const ratingCollection=client.db('carRating').collection('rating')
+        const userCollection=client.db('carUser').collection('user')
+
+
+// ...............................CREATE....................................
 
         // Create service (POST)
         app.post("/service",async(req, res) => {
@@ -47,6 +50,9 @@ async function run(){
         res.json(result);
       });
         
+
+    //   ............................READ...................................................
+
         // getting service
         app.get("/service",async(req,res)=>{
           const query={}
@@ -54,16 +60,35 @@ async function run(){
           const services=await cursor.toArray()
           res.send(services)
         });
+
+
+         // Get Single service
+         app.get("/service/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log("getting specific service", id);
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+          });
         
+        // user
         
-        // Get Single package
-        app.get("/service/:id", async (req, res) => {
-          const id = req.params;
-          console.log("getting specific service", id);
-          const query = { _id: ObjectId(id) };
-          const service = await serviceCollection.findOne(query);
-          res.send(service);
+        app.get("/user",async(req,res)=>{
+          const query={}
+          const cursor=userCollection.find(query)
+          const users=await cursor.toArray()
+          res.send(users)
         });
+        
+        // order
+
+        app.get("/order",async(req,res)=>{
+            const query={}
+            const cursor=orderCollection.find(query)
+            const orders=await cursor.toArray()
+            res.send(orders)
+          });
+
 
         // get orders by email
 
@@ -74,6 +99,15 @@ async function run(){
             console.log(order);
             res.json(order);
           });
+
+         // Check Admin
+          app.get("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const user = await userCollection.findOne(filter);
+            console.log(user);
+            res.json(user);
+          });
         
           //Get Rating
     app.get("/rating", async (req, res) => {
@@ -82,6 +116,40 @@ async function run(){
         res.json(rating);
       });
 
+    //   ..........................UPDATE................................
+
+    //   user upsert
+app.put('/user/:email', async (req, res) => {
+const email=req.params.email;
+const user = req.body;
+const filter={email:email};
+const options={upsert:true};
+const updateDocument = {
+    $set: user,
+};
+  const result = await userCollection.updateOne(
+    filter,
+    updateDocument,
+    options
+  );
+  res.send(result);
+
+});
+
+ //make admin
+ app.put("/user/makeAdmin/:email", async (req, res) => {
+     const email = req.params.email;
+     console.log(email);
+    const filter = { email: email };
+    const updateDoc = { 
+        $set: { role: "admin" }, 
+    };
+    const result = await userCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  });
+
+
+//   ....................DELETE..........................
 
        // DELETE a service
     app.delete("/service/:id", async (req, res) => {
